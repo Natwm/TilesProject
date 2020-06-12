f2b3d66a6bb4eb7ed3f78ac7292980a85436aa50
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2020-06-12 18:44:56 +0200

    Assets
    lololol

---
+++ projetS1/Projet Semestre 1/Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public float radius = 1;
+	public Vector2 regionSize = Vector2.one;
+	public int rejectionSamples = 30;
+	public float displayRadius =1;
+    GridGen grid;
+	List<Vector2> points;
+    private void Start()
+    {
+        grid = FindObjectOfType<GridGen>();
+        regionSize.x = grid.originCorner.x;
+        regionSize.y = grid.farCorner.y;
+    }
+    void OnValidate() {
+		points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectionSamples);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(regionSize/2,regionSize);
+		if (points != null) {
+			foreach (Vector2 point in points) {
+				Gizmos.DrawSphere(point, displayRadius);
+			}
+		}
+	}
+}
