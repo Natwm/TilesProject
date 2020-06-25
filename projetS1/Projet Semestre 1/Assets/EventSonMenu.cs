e70b16586f7e7f00a5af5052101b700d1e6233cf
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2020-06-25 21:51:15 +0200

    SuiteFMOD

--- projetS1/Projet Semestre 1/Assets/EventSonMenu.cs
+++ projetS1/Projet Semestre 1/Assets/EventSonMenu.cs
@@ -4,28 +4,15 @@ using UnityEngine;
 
 public class EventSonMenu : MonoBehaviour
 {
-    FMODUnity.StudioEventEmitter myEmit;
+   public FMODUnity.StudioEventEmitter myEmit;
 
     [FMODUnity.EventRef]
     public string iWantToPlayCustomEvent;
 
     FMOD.Studio.EventInstance mySoundToPlay;
     // Start is called before the first frame update
-    void Start()
-    {
-        myEmit = GetComponent<FMODUnity.StudioEventEmitter>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.K))
-        {
-            mySoundToPlay = FMODUnity.RuntimeManager.CreateInstance(iWantToPlayCustomEvent);
-            myEmit.Event = iWantToPlayCustomEvent;
-            myEmit.Play();
-
-        }
-    }
+   
+   
     public void playSound()
     {
         if (myEmit != null)
