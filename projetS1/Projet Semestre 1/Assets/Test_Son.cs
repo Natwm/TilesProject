f8a2f7047d6ec94fec44df3948c6a33d6de72756
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2020-06-25 19:36:24 +0200

    Fmod Push + Menu

---
+++ projetS1/Projet Semestre 1/Assets/Test_Son.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Son : MonoBehaviour
+{
+
+
+    FMODUnity.StudioEventEmitter TestEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestEvent = GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+    // Update is called once per frame
+  
+}
---
+++ projetS1/Projet Semestre 1/Assets/shrubRender.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shrubRender : MonoBehaviour
+{
+    PoissonInterface poisson;
+    public Sprite shrubToRender;
+    MeshRenderer shrubMeshRender1;
+    MeshRenderer shrubMeshRender2;
+    public Sprite[] shrubChoice;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // shrubChoice = new Sprite[poisson.shrubs.Length];
+        
+        shrubMeshRender1 = transform.GetChild(0).GetComponent<MeshRenderer>();
+        shrubMeshRender2 = transform.GetChild(1).GetComponent<MeshRenderer>();
+    }
+
+   public void setShrub()
+    {/*
+        poisson = FindObjectOfType<PoissonInterface>();
+        shrubChoice = poisson.shrubs;
+        int RandomShrub = Random.Range(0, shrubChoice.Length-1);
+        shrubToRender = shrubChoice[RandomShrub];
+        shrubMeshRender1.material.mainTexture = shrubToRender.texture;
+        shrubMeshRender2.material.mainTexture = shrubToRender.texture;
+    */
+    } 
+}
